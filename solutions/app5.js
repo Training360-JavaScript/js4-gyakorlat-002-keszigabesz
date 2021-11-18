fa3a5aed76c08bdad4bda75e53f57b78626bea76
keszigabesz <bajo.gabor@gmail.com>
2021-11-18 19:32:20 +0100

    kész

--- solutions/app5.js
+++ solutions/app5.js
@@ -10,7 +10,14 @@ const getProductData = () => productData;
  * @returns {[{name: string}]} a name alapján rendezett tömb
  */
 const sortProducts = (products = [{ name: '' }]) => {
-    // itt dolgozz
+    const sortedData = products.sort((a, b)=>{
+        let x = a.name.toLowerCase();
+        let y = b.name.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+      });
+    productData=sortedData;
 };
 
 /**
